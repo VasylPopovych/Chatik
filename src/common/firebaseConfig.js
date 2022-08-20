7aff97df3908fc3f821b0344dc9621f0e3697fcb
Vasyl Popovych <popovich.w.ppc@gmail.com>
2022-08-20 12:27:57 +0300

    add scss module and firebase to app

---
+++ src/common/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBOZaZ39q16Rg0LvhhkRJHFlC_mi3N5IXI",
+  authDomain: "chatik-a75f0.firebaseapp.com",
+  projectId: "chatik-a75f0",
+  storageBucket: "chatik-a75f0.appspot.com",
+  messagingSenderId: "1093247609464",
+  appId: "1:1093247609464:web:e3debd55fe349a9b9f9b8d",
+  measurementId: "G-LMN79N2GNK",
+};
+
+export default firebaseConfig;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles/index.css";
+import "./styles/index.scss";
 import App from "./App";
+import { FirebaseAppProvider } from "reactfire";
+import firebaseApp from "./common/firebaseApp";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <FirebaseAppProvider firebaseApp={firebaseApp}>
+      <App />
+    </FirebaseAppProvider>
   </React.StrictMode>
 );
