59df510060469148ee1efbb8e736a6c8dbd01e28
Vasyl Popovych <popovich.w.ppc@gmail.com>
2022-08-21 16:15:52 +0300

    configure styles and layout on notFoundPage

--- src/components/pages/NotFoundPage.js
+++ src/components/pages/NotFoundPage.js
@@ -1,9 +1,16 @@
-import {} from "../../styles/notFoundPage.scss";
+import { useNavigate } from "react-router-dom";
 
 const NotFoundPage = () => {
+  let navigate = useNavigate();
+
+  const toHomePage = () => {
+    navigate("/");
+  };
+  // in future: create button "Go to Home"!
   return (
     <div className="notFoundPage">
       <h1>Page Not Found</h1>
+      <button onClick={toHomePage}>Home Page</button>
     </div>
   );
 };
